fdcd540242ceb4134c58f2599836d79b8e1b9585
VikasVats1998 <vvats888@gmail.com>
2021-10-22 22:48:38 -0700

    Create table and insert data into table

---
+++ insertDataIntoTable.php
@@ -0,0 +1,13 @@
+<?php
+$server='localhost';
+$user='root';
+$passward='';
+$db='phppdo';
+
+$dbcon = new PDO("mysql:host=$server; dbname=$db",$user,$passward);
+
+$insertquery = " insert into student(name,age,class,gender) value('Vikas',22,MCA,male) ";
+// You must have to create table having the following columns : name, age, class, gender 
+
+$dbcon->query($insertquery);
+?>
